dea6e0f0e5f43e32da1e7a457773ccd0ed012869
zodd18 <josiesala@gmail.com>
2020-12-04 21:55:32 +0100

    Minor changes

--- .config/neofetch/launch-neofetch.sh
+++ .config/neofetch/launch-neofetch.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-/bin/neofetch --ascii $HOME/.config/neofetch/ascii --ascii_colors 5 --colors 6 7 7 6 6 7
-#/bin/neofetch --backend kitty --source ~/.config/neofetch/image.png --size '270px'
+/bin/neofetch --backend kitty --source $HOME/.config/neofetch/image.png --size '200px' --colors 5 5 7 4 4 7
